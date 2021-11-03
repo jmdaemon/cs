a3e36e7712d50361f6d733f04f89c12efd727003
Joseph Diza <josephm.diza@gmail.com>
2021-11-02 20:07:10 -0700

    Remove dead code in cs

--- src/cs.py
+++ src/cs.py
@@ -1,23 +1,10 @@
-#!/usr/bin/python
-
-# graph.py - Graph mathematical equations and functions
-
-import matplotlib.pyplot as plt
-import math
-import numpy as np
 import argparse
-# import click
-# import argh
+import math
+import matplotlib.pyplot as plt
+import numpy as np
 
+# cs.py - Graph mathematical equations and functions
 
-# CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
-
-# @click.option('-V'   , '--version'      , 'version'     , help='Show program version'           , is_flag=True, default=False)
-# @click.option('-v'   , '--verbose'      , 'verbose'     , help='Display verbose output'         , is_flag=True, default=False)
-# @click.argument('inp', metavar='<inp>', required=True)
-# @click.command(options_metavar='[options]', context_settings=CONTEXT_SETTINGS)
-# def cli(version, verbose, inp):
-# def main(inp, beg=-10, end=10, n=1000, version=False, verbose=False):
 def main():
     parser = argparse.ArgumentParser(description='Executable file for Labs')
     parser.add_argument('input' , type=str, help='File path to some input.csv')
@@ -30,25 +17,10 @@ def main():
     beg, end    = args.range
     n           = args.n
 
-    # if (version):
-        # MAJOR, MINOR, PATCH = '0', '0', '1'
-        # print(f'strmanip - v{MAJOR}.{MINOR}.{PATCH}')
-        # return
-
-    # create 1000 equally spaced points between -10 and 10
-    # x = np.linspace(-10, 10, 1000)
     x = np.linspace(beg, end, n)
 
-    # calculate the y value for each element of the x vector
     fn = eval(inp)
-    # fn = exec(inp)
 
     fig, ax = plt.subplots()
     ax.plot(x, fn)
     plt.show()
-
-
-# if __name__ == '__main__':
-    # cli()
-
-# argh.dispatch_command(main)
